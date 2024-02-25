6a2708fec69920e18fa385ab0e3c45f116f03e29
Minh-Khoi <nguyenxngocminhkhoi@gmail.com>
2024-02-25 12:21:22 +0700

    commit assignment

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const bodyParser = require('body-parser');
+const multer = require('multer');
+require('dotenv').config();
 
 var app = express();
 
 app.use(cors());
+// Configure body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+// Configure multer storage
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let file = req.file;
+  let resObj = {
+    'name': file.originalname, 'type': file.mimetype, 'size': file.size
+  };
+  res.send(resObj);
+});
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
